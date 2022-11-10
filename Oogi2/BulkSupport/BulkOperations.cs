04a04405515cb2290855f2c3c52b66e5e7f0da0e
frohikey <frohikey@gmail.com>
2022-11-10 16:56:03 +0100

    Bulk ops with dop

--- Oogi2/BulkSupport/BulkOperations.cs
+++ Oogi2/BulkSupport/BulkOperations.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Oogi2.BulkSupport
@@ -19,13 +20,44 @@ namespace Oogi2.BulkSupport
         public async Task<BulkOperationResponse<T>> ExecuteAsync()
         {
             await Task.WhenAll(Tasks);
+
             stopwatch.Stop();
+            
             return new BulkOperationResponse<T>()
             {
-                TotalTimeTaken = stopwatch.Elapsed,                
+                TotalTimeTaken = stopwatch.Elapsed,
                 SuccessfulItems = Tasks.Count(task => task.Result.IsSuccessful),
                 Failures = Tasks.Where(task => !task.Result.IsSuccessful).Select(task => (task.Result.Item, task.Result.CosmosException)).ToList()
             };
         }
+
+        public async Task<BulkOperationResponse<T>> ExecuteAsync(int dop)
+        {
+            using (var semaphore = new SemaphoreSlim(initialCount: dop, maxCount: dop))
+            {
+                var tasks = Tasks.Select(async item =>
+                {
+                    await semaphore.WaitAsync();
+                    try
+                    {
+                        await item;
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                });
+                await Task.WhenAll(tasks);
+            }
+
+            stopwatch.Stop();
+
+            return new BulkOperationResponse<T>()
+            {
+                TotalTimeTaken = stopwatch.Elapsed,
+                SuccessfulItems = Tasks.Count(task => task.Result.IsSuccessful),
+                Failures = Tasks.Where(task => !task.Result.IsSuccessful).Select(task => (task.Result.Item, task.Result.CosmosException)).ToList()
+            };
+        }        
     }
 }
